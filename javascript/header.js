71713c3ca7b9ec4ee36a662d820817ebdb36f6dd
IrynkaZozuliak <irynka.zozuliak@gmail.com>
2024-08-20 18:35:53 +0000

    hhh

--- javascript/header.js
+++ javascript/header.js
@@ -1,34 +1,29 @@
- 
-hamburger = document.querySelector(".hamburger");
+hamburger = document.querySelector(".header__hamburger");
 hamburger.onclick = function () {
-    navBar = document.querySelector(".header__nav-bar");
-    navBar.classList.toggle("active");
-} 
+  navBar = document.querySelector(".header__nav-bar");
+  navBar.classList.toggle("active");
+};
 //чи потрібний код вище для гамбургер меню???
-
-
 
 //цей код працює добре
 // Отримання посилань меню
-var menuLinks = document.querySelectorAll('.nav__menu-link');
+var menuLinks = document.querySelectorAll(".header__menu-link");
 
 // Додавання обробника подій для кожного посилання
-menuLinks.forEach(function(link) {
-link.addEventListener('click', function(event) {
-// Згортання меню
-var navBar = document.querySelector(".header__nav-bar");
-navBar.classList.remove("active");
-});
+menuLinks.forEach(function (link) {
+  link.addEventListener("click", function (event) {
+    // Згортання меню
+    var navBar = document.querySelector(".header__nav-bar");
+    navBar.classList.remove("active");
+  });
 });
 
 // Отримання кнопки гамбургера
-var hamburger = document.querySelector(".hamburger");
+var hamburger = document.querySelector(".header__hamburger");
 
 // Додавання обробника подій для кнопки гамбургера
 hamburger.onclick = function () {
-// Розгортання або згортання меню
-var navBar = document.querySelector(".header__nav-bar");
-navBar.classList.toggle("active");
+  // Розгортання або згортання меню
+  var navBar = document.querySelector(".header__nav-bar");
+  navBar.classList.toggle("active");
 };
-
- 
